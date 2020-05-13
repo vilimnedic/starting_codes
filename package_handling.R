56bb2c528553573d763864d2475b1be9376f1689
Vilim <48656717+vilimnedic@users.noreply.github.com>
2020-05-13 20:13:05 +0200

    Update package_handling.R

--- package_handling.R
+++ package_handling.R
@@ -26,10 +26,23 @@ package.check <- lapply(
 # Step 3: Look up the package in CRAN-mirror and see if you have wrong client
 # library(installr)
 # updateR()
+# Cred: Richie Cotton @ 
+# https://stackoverflow.com/questions/25721884/how-should-i-deal-with-package-xxx-is-not-available-for-r-version-x-y-z-wa 
 
 # Detach packages
 # detach("package:dplyr", unload = TRUE)
 
 # Check if packages are loaded
+# Solution A
 # loaded_packages <- library()$results[, 1]
 # "package_name" %in% tolower(loaded_packages)
+# Solution B
+# (.packages())
+# "dplyr" %in% tolower((.packages()))
+# Solution C
+# And load it if it's not loaded yet
+# if(! "dplyr" %in% tolower((.packages()))){
+#   library("dplyr")
+#   (.packages())
+# }
+# Cred: https://rpubs.com/Mentors_Ubiqum/list_packages
